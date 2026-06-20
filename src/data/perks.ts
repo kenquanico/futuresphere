feb49c0fdb3bcd132ff26edbfc14cf68bc6d545c
kenquan <nekquanico@gmail.com>
2026-06-20 13:03:23 +0800

    initialize project structure with Vue, Vite, and essential components

---
+++ src/data/perks.ts
@@ -0,0 +1,32 @@
+export const perks = [
+  {
+    title: 'Flexible Work Environment',
+    description:
+      'Enjoy the freedom to work from anywhere in the world, whether it is the comfort of your home office or the excitement of a new city.',
+  },
+  {
+    title: 'Cultural Diversity',
+    description:
+      'Experience the richness of working with a global team. Collaborate with professionals from different backgrounds and perspectives.',
+  },
+  {
+    title: 'Professional Development',
+    description:
+      'Gain access to a wide range of learning and development opportunities, from training programs to conferences.',
+  },
+  {
+    title: 'Travel Opportunities',
+    description:
+      'Embark on exciting journeys and visit different locations as part of your job. Engage in on-site collaborations and conferences.',
+  },
+  {
+    title: 'Social Networking',
+    description:
+      'Connect with professionals from around the world and build a vast network. Exchange ideas and create meaningful connections.',
+  },
+  {
+    title: 'Competitive Compensation',
+    description:
+      'Benefit from a competitive compensation package that recognizes your skills and contributions.',
+  },
+];
