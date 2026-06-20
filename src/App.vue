c835a23891d9adfbb0258eeace098e8fe76f1a1e
kenquan <nekquanico@gmail.com>
2026-06-20 13:00:34 +0800

    initialize project structure with Vue, Vite, and essential components

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <AppNavbar />
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import AppNavbar from './components/shared/AppNavbar.vue';
+</script>
