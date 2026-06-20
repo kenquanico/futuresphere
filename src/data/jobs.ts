1e5189af203e6580833329b55345eb5bc1163705
kenquan <nekquanico@gmail.com>
2026-06-20 13:02:23 +0800

    initialize project structure with Vue, Vite, and essential components

---
+++ src/data/jobs.ts
@@ -0,0 +1,64 @@
+export const jobs = [
+  {
+    id: 1,
+    slug: 'product-manager',
+    title: 'Product Manager',
+    type: 'Full Time',
+    location: 'On site',
+    city: 'San Francisco',
+    department: 'Design',
+    description:
+      'FutureSphere is in need of a talented Product Designer with more than 3 years of past experience in tech sector.',
+  },
+  {
+    id: 2,
+    slug: 'human-resources',
+    title: 'Human Resources',
+    type: 'Full Time',
+    location: 'On site',
+    city: 'San Francisco',
+    department: 'People',
+    description:
+      'FutureSphere is in need of a talented Product Designer with more than 3 years of past experience in tech sector.',
+  },
+  {
+    id: 3,
+    slug: 'ui-ux-designer',
+    title: 'UI/UX Designer',
+    type: 'Full Time',
+    location: 'Remote',
+    city: 'Remote',
+    department: 'Design',
+    description:
+      'FutureSphere is in need of a talented Product Designer with more than 3 years of past experience in tech sector.',
+  },
+];
+
+export const jobDetails = {
+  education: [
+    "Bachelor's degree in Business Administration, Computer Science, Engineering, or a related field.",
+    'Proven experience of at least 3 years in product management or related roles, preferably in the technology industry.',
+    'Experience with Agile development methodologies and product lifecycle management.',
+  ],
+  technical: [
+    'Strong understanding of product development processes and principles.',
+    'Proficiency using project management tools such as JIRA or Asana.',
+    'Familiarity with prototyping tools like Sketch, Adobe XD, or InVision.',
+    'Knowledge of data analysis and visualization tools such as Excel, Tableau, or Google Analytics.',
+    'Familiarity with HTML, CSS, and JavaScript.',
+  ],
+  competencies: [
+    'Strategic Thinking: ability to analyze market trends, customer needs, and business goals.',
+    'Leadership: guide cross-functional teams and stakeholders in delivering successful products.',
+    'Communication: convey product vision, requirements, and updates to stakeholders.',
+    'Problem-Solving: identify challenges, devise solutions, and make data-driven decisions.',
+    'Collaboration: align product strategy with business objectives across teams.',
+  ],
+  qualities: [
+    'Passion for technology and emerging trends.',
+    'Customer-centric mindset and appetite for user research.',
+    'Adaptability in a fast-paced, dynamic environment.',
+    'Strong attention to detail and product quality.',
+    'Results-driven track record of delivering successful products.',
+  ],
+};
